7a13dffa41d9f0faa64c292c483213c2cf2c907a
E.Becheva <34000861+egbecheva@users.noreply.github.com>
2022-12-20 22:34:59 +0200

    Add React router

--- src/JobDetails.tsx
+++ src/JobDetails.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Grid, Container } from '@mui/material';
+import { useLocation } from 'react-router-dom';
 
 const JobDetails: React.FC<{}> = ({}) => {
+  const {
+    state: { company_name },
+  } = useLocation();
   return (
     <div>
       <Container>
         <Grid container>
           <Grid item xs={12} md={4}>
             <div>Back to Home</div>
+            <div>{company_name} bab </div>
             <div>How to apply</div>
           </Grid>
           <Grid item xs={12} md={8}>
